bbe9f7167261ac8919e30138405764ba8562f61f
jandreeb <jandreeb@icloud.com>
2026-08-01 22:56:31 -0400

    Create plantillas.js

---
+++ plantillas.js
@@ -0,0 +1,28 @@
+// ===============================
+// PLANTILLAS DE REPORTES
+// ===============================
+
+function crearAmbulante(datos){
+
+    return `*Procedimiento:*
+Desalojo de comercio ambulante informal
+
+*Detalles del procedimiento:*
+A las ${datos.hora} hrs, con ${datos.detectado.toLowerCase()} se procede a desalojar del mercado a una persona, la cual se encontraba realizando comercio ambulante no autorizado (venta de ${datos.especie}). Se entregan verbalmente las normativas del mercado, quedando en conocimiento de la falta cometida, lo que motivó su desalojo sin más novedades.
+
+*Calle/Postura/Patio:*
+${datos.lugar}, ${datos.patio}
+
+*Desalojada por Puerta:*
+${datos.puerta}
+
+*Procedimiento realizado por:*
+*${datos.nombre} - ${datos.indicativo} - ${datos.patio}*
+
+*Nombre Jefe Cuadrante:*
+*${datos.jefeCuadrante}*
+
+*Nombre Jefe de Mercado:*
+*${datos.jefeMercado}*`;
+
+}
