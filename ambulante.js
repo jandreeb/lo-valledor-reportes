c3d911f4623a3c08ed9d3feea9ec835b6044ed04
jandreeb <jandreeb@icloud.com>
2026-08-01 22:21:12 -0400

    Update ambulante.js

--- ambulante.js
+++ ambulante.js
@@ -1,1 +1,35 @@
+// ===============================
+// VENTANA AMBULANTE
+// ===============================
 
+const btnAmbulante = document.getElementById("btnAmbulante");
+const formAmbulante = document.getElementById("formAmbulante");
+const cerrarAmbulante = document.getElementById("cerrarAmbulante");
+
+if(btnAmbulante){
+
+    btnAmbulante.addEventListener("click", ()=>{
+
+        formAmbulante.style.display="block";
+
+        const ahora = new Date();
+
+        const horas = String(ahora.getHours()).padStart(2,"0");
+        const minutos = String(ahora.getMinutes()).padStart(2,"0");
+
+        document.getElementById("horaAmbulante").value =
+        `${horas}:${minutos}`;
+
+    });
+
+}
+
+if(cerrarAmbulante){
+
+    cerrarAmbulante.addEventListener("click", ()=>{
+
+        formAmbulante.style.display="none";
+
+    });
+
+}
