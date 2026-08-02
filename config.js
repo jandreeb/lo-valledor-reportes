ce1cc956ebb3a65afcec36fd4f09af167bd71819
jandreeb <jandreeb@icloud.com>
2026-08-01 21:44:29 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+// ===================================
+// CONFIGURACIÓN GENERAL LV ASSISTANT
+// ===================================
+
+const CONFIG = {
+
+    nombre: "José Andrés Bolaños Vera",
+
+    empresa: "Lo Valledor",
+
+    version: "3.0 Beta"
+
+};
+
+console.log("CONFIG cargada correctamente.");
