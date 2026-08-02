e22eb3a3ba56d91922f0e100ab6418c9732b0c8d
jandreeb <jandreeb@icloud.com>
2026-08-01 23:52:45 -0400

    Update utilidades.js

--- utilidades.js
+++ utilidades.js
@@ -1,1 +1,11 @@
+// ===============================
+// UTILIDADES
+// ===============================
 
+function copiarReporte(texto){
+
+    navigator.clipboard.writeText(texto);
+
+    alert("✅ Procedimiento copiado al portapapeles.");
+
+}
